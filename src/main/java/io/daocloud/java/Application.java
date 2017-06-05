6578eaa293da3e12cbade1a263f9d896d09b1fee
yann <me.yan.xia@qq.com>
2017-06-05 18:18:34 +0800

    initial commit

---
+++ src/main/java/io/daocloud/java/Application.java
@@ -0,0 +1,31 @@
+package io.daocloud.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yann on 2017/6/5.
+ */
+
+
+@RestController
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+
